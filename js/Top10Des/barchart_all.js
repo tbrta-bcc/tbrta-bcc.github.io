0e7a1e082da99d9bfa99ff5d673cd8c4c218669b
Xi Li <lixi220@outlook.com>
2020-09-01 15:00:19 -0400

    add TAZ to top-10 map

--- js/Top10Des/barchart_all.js
+++ js/Top10Des/barchart_all.js
@@ -1,6 +1,7 @@
 //Travel Flow Bar Chart	
 
 // Initialize the chart	
+
 		var ctx = document.getElementById("myChart").getContext("2d");
 
 		var myBarChart = new Chart(ctx, {
@@ -8,31 +9,31 @@
 			data: {
 				labels: ["Top1", "Top2", "Top3", "Top4", "Top5", "Top6","Top7","Top8","Top9","Top10"],
 				datasets: [{
-					label: 'Daily Total All-Purpose Trips',
+					label: barchartLabel,
 					data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 					backgroundColor: [
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)',
-						'rgba(255, 159, 64, 0.2)'
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)'
 					],
 					borderColor: [
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)',
-						'rgba(255, 159, 64, 1)'
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)'
 					],
 					borderWidth: 1
 				}]
